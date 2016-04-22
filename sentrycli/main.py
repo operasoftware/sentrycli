74c8f2ca173f8c5f38b09b0c85b7cbb1636336ce
Michał Łowicki <mlowicki@gmail.com>
2016-04-22 15:22:33 +0200

    Introduce --version

--- sentrycli/main.py
+++ sentrycli/main.py
@@ -1,14 +1,20 @@
+import argparse
 import logging
 
+from argh.constants import PARSER_FORMATTER
 import argh
 
-
+from sentrycli import __version__
 from sentrycli.group import group
 from sentrycli.query import query
 
 
 def main():
-    argh.dispatch_commands([query, group])
+    parser = argparse.ArgumentParser(formatter_class=PARSER_FORMATTER)
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s ' + __version__)
+    argh.add_commands(parser, [query, group])
+    argh.dispatch(parser)
 
 
 if __name__ == '__main__':
